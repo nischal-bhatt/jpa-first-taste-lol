a48ad0273b3d505448dcdc2684d2abdbde4815b9
nischal-bhatt <nischal.bhatt.87@gmail.com>
2023-02-26 17:30:43 +0800

    asdad

---
+++ spring-boot-h2-2/src/main/java/com/example/demo/controllerClass.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controllerClass {
+
+	@Autowired
+	private StudentRepository studentRepo;
+	
+	@GetMapping("/hi")
+	public void shit()
+	{
+		System.out.println("hey");
+	}
+	
+	@GetMapping("/bhatt")
+	public List<Student> getshits(@RequestParam("bb") String name)
+	{
+		return this.studentRepo.rraj(name);
+	}
+}
